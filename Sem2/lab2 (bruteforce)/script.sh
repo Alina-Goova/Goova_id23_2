790f579d3cd535c550fd6896578b97815ef9c6c8
Alina-Goova <147145257+Alina-Goova@users.noreply.github.com>
2025-04-14 23:12:36 +0300

    Add files via upload

---
+++ Sem2/lab2 (bruteforce)/script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+git clone --recurse-submodules https://github.com/openwall/john.git
+sudo apt-get update
+sudo apt-get install -y build-essential libssl-dev zlib1g-dev
+sudo apt install -y hashcat
+cd john/src
+./configure
+make -s clean && make -sj4
+cd ../..
+python3 main.py
