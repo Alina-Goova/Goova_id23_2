399c77abbb142c38f51384e9de1cf5fa4b95f556
Alina-Goova <147145257+Alina-Goova@users.noreply.github.com>
2025-06-07 22:06:45 +0300

    Update script.sh

--- Sem2/lab2 (bruteforce)/script.sh
+++ Sem2/lab2 (bruteforce)/script.sh
@@ -5,6 +5,4 @@ sudo apt-get install -y build-essential libssl-dev zlib1g-dev
 sudo apt install -y hashcat
 cd john/src
 ./configure
-make -s clean && make -sj4
-cd ../..
-python3 main.py
+make -s clean && make -sj8
